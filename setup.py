899494e2482a4e826d4c5c33f7943b3c270c764a
Byishimo Teto Joseph <tetobobo1@gmail.com>
2025-07-18 14:29:08 +0200

    (feat) support format selection

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='kn-yt-downloader',
-    version='0.1.0',
-    description='Knight YouTube Downloader CLI Tool',
-    author='Knight',
-    author_email='knight01@gmail.com',
+    name="kn-yt-downloader",
+    version="0.1.0",
+    description="Knight YouTube Downloader CLI Tool",
+    author="Knight",
+    author_email="knight01@gmail.com",
     packages=find_packages(),
     install_requires=[
-        'yt-dlp',
-        'rich',
+        "yt-dlp",
+        "rich",
     ],
     entry_points={
-        'console_scripts': [
-            'kn-dl=kn_yt_downloader.cli:main'
-        ],
+        "console_scripts": ["kn-dl=kn_yt_downloader.cli:main"],
     },
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Environment :: Console',
-        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
